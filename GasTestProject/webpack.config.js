295fcde7ad4e68003c339d929bf5144575f2ca3b
max-eipi <psp.dsi.wii@gmail.com>
2018-06-23 02:41:12 +0900

    設定ファイルの作成・編集

---
+++ GasTestProject/webpack.config.js
@@ -0,0 +1,28 @@
+﻿const path = require('path');
+const GasPlugin = require("gas-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: './src/app.ts',
+    devtool: false,
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: [
+            '.ts'
+        ]
+    },
+    plugins: [
+        new GasPlugin()
+    ]
+};
